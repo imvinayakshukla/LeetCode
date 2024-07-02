c5b11b76b8e2035aedc440f8e4355182880f1c9a
Vinayak Shukla <vinayakshukla0786@gmail.com>
2024-07-02 23:44:56 +0530

    Time: 5 ms (23.85%), Space: 43.4 MB (21.47%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,40 +1,32 @@
 class Solution {
     public int[] intersect(int[] nums1, int[] nums2) {
-        ArrayList <Integer> list=new ArrayList<>();
-        int k=0;
+        ArrayList<Integer> list = new ArrayList<>();
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        int k = 0;
         Arrays.sort(nums1);
-
         Arrays.sort(nums2);
 
-        int l1=0,l2=0;
+        for (int i = 0; i < nums2.length; i++) {
+            mp.put(nums2[i], mp.getOrDefault(nums2[i], 0) + 1);
 
-        while(l1 < nums1.length && l2 < nums2.length)
-        {
-
-            if(nums1[l1] == nums2[l2])
-            {
-                list.add(nums1[l1]);
-                l1++;
-                l2++;
-
-            }
-            else if(nums1[l1] > nums2[l2])
-            {
-                l2++;
-                
-            }
-            else if(nums1[l1] < nums2[l2])
-            {
-                l1++;
-                
-            }
-            
         }
-        int ans[]=new int[list.size()];
-        for(int i : list)
-        {
-            ans[k++]=i;
+
+        for (int i = 0; i < nums1.length; i++) {
+            if(mp.containsKey(nums1[i]))
+            {
+                if(mp.getOrDefault(nums1[i], 0) > 0 )
+                list.add(nums1[i]);
+                mp.put(nums1[i], mp.getOrDefault(nums1[i], 0) - 1);
+            }
+
         }
+
+        int ans[] = new int[list.size()];
+        for (int i : list) {
+            ans[k++] = i;
+        }
+
         return ans;
+
     }
 }
