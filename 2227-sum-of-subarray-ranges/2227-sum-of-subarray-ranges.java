51da0eec92b28ef9324c37c9ea3981e293e93a9d
Vinayak Shukla <vinayakshukla0786@gmail.com>
2024-07-19 00:19:19 +0530

    Time: 24 ms (25.94%), Space: 43.9 MB (84.7%) - LeetHub

--- 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/2227-sum-of-subarray-ranges.java
@@ -1,17 +1,103 @@
 class Solution {
-    public long subArrayRanges(int[] nums) {
-        long sum = 0;
+     public long sumSubarrayMins(int[] arr) {
+        int sum=0;
+        Stack<Integer> st = new Stack<>();
+        int left[]=new int[arr.length];
+        int right[]=new int[arr.length];
 
-        for (int i = 0; i < nums.length; i++) {
-            int small = nums[i], large = nums[i];
-            for (int j = i + 1; j < nums.length; j++) {
-                small = Math.min(small, nums[j]);
-                large = Math.max(large, nums[j]);
-                sum += large - small;
-            }
+        Arrays.fill(left, -1);
+        Arrays.fill(right, arr.length);
+
+        for(int i=0;i<arr.length;i++)
+        {   
+            while( !st.isEmpty() && arr[st.peek()] >= arr[i] )
+            
+            st.pop();
+
+            if(!st.isEmpty())
+            left[i]=st.peek();
+
+            st.push(i);
+
 
         }
-        return sum;
+        st.clear();
+         for(int i=arr.length-1;i>=0;i--)
+        {   
+            while(!st.isEmpty() && arr[st.peek()] > arr[i])
+            st.pop();
 
+            if(!st.isEmpty())
+            right[i]=st.peek();
+
+            st.push(i);
+
+
+        }
+     
+        
+        long ans=0;
+
+         for (int i = 0; i < arr.length; ++i) {
+            ans+=(long) (i - left[i]) * (right[i] - i)  * arr[i] ;
+            
+
+         }
+
+         return  ans;
+        
+    }
+
+     public long sumSubarrayMax(int[] arr) {
+        int sum=0;
+        Stack<Integer> st = new Stack<>();
+        int left[]=new int[arr.length];
+        int right[]=new int[arr.length];
+
+        Arrays.fill(left, -1);
+        Arrays.fill(right, arr.length);
+
+        for(int i=0;i<arr.length;i++)
+        {   
+            while( !st.isEmpty() && arr[st.peek()] <= arr[i] )
+            
+            st.pop();
+
+            if(!st.isEmpty())
+            left[i]=st.peek();
+
+            st.push(i);
+
+
+        }
+        st.clear();
+         for(int i=arr.length-1;i>=0;i--)
+        {   
+            while(!st.isEmpty() && arr[st.peek()] < arr[i])
+            st.pop();
+
+            if(!st.isEmpty())
+            right[i]=st.peek();
+
+            st.push(i);
+
+
+        }
+     
+       
+        long ans=0;
+
+         for (int i = 0; i < arr.length; ++i) {
+            ans+=(long) (i - left[i]) * (right[i] - i)  * arr[i] ;
+           
+
+         }
+
+         return  ans;
+        
+    }
+    public long subArrayRanges(int[] nums) {
+        return sumSubarrayMax(nums)-sumSubarrayMins(nums);
+        
     }
 }
