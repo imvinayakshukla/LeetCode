9c3c0cbf5394d1625c1539abe18431c9fb5ac1d8
Vinayak Shukla <vinayakshukla0786@gmail.com>
2024-08-11 12:03:44 +0530

    Time: 0 ms (100%), Space: 41.3 MB (15.92%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -7,17 +7,15 @@ class Solution {
     }
 
     private int solve(int[] nums, int start, int end) {
-        int[] dp = new int[end - start + 1];
+        int prev1 = 0;
+        int prev2 = 0;
 
-        dp[0] = nums[start];
-        if (dp.length > 1) {
-            dp[1] = Math.max(nums[start], nums[start + 1]);
+        for (int i = start; i <= end; i++) {
+            int current = Math.max(prev1, prev2 + nums[i]);
+            prev2 = prev1;
+            prev1 = current;
         }
 
-        for (int i = 2; i < dp.length; i++) {
-            dp[i] = Math.max(nums[start + i] + dp[i - 2], dp[i - 1]);
-        }
-
-        return dp[dp.length - 1];
+        return prev1;
     }
 }
