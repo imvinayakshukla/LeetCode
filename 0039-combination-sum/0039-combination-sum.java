75638b1a32aac449b8b1cfbb02bbf8c9330a4c0e
Vinayak Shukla <vinayakshukla0786@gmail.com>
2024-08-14 10:53:32 +0530

    Time: 1 ms (99.93%), Space: 44.5 MB (65.69%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,29 @@
+class Solution {
+   static List<List<Integer>> ans;
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        ans = new ArrayList<>();
+        cal(0, candidates, target, new ArrayList<Integer>());
+        return ans;
+
+    }
+
+    public static void cal(int inx, int[] arr, int target, ArrayList<Integer> ds) {
+         if (target == 0) {
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+        if (inx == arr.length) {
+            return;
+        }
+
+        if (arr[inx] <= target) {
+            ds.add(arr[inx]);
+            cal(inx, arr, target - arr[inx], ds);
+              ds.remove(ds.size() - 1);
+
+        }
+        cal(inx + 1, arr, target, ds);
+
+    }
+}
