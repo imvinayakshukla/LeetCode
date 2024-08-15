3e73a1fd4a7521cfa48cd755c49105bd48ee43e1
Vinayak Shukla <vinayakshukla0786@gmail.com>
2024-08-15 10:39:46 +0530

    Time: 0 ms (100%), Space: 41.7 MB (27.03%) - LeetHub

--- 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.java
+++ 2316-count-hills-and-valleys-in-an-array/2316-count-hills-and-valleys-in-an-array.java
@@ -1,7 +1,8 @@
 class Solution {
     public int countHillValley(int[] nums) {
         int n = nums.length;
-        if (n == 0) return 0;
+        if (n == 0)
+            return 0;
 
         int count = 0;
         int left = 0;
@@ -12,9 +13,9 @@ class Solution {
                 right++;
                 continue;
             }
-
-            if ((nums[right - 1] > nums[left] && nums[right - 1] > nums[right]) ||
-                (nums[right - 1] < nums[left] && nums[right - 1] < nums[right])) {
+            int curr=right - 1;
+            if ((nums[curr] > nums[left] && nums[curr] > nums[right]) ||
+                    (nums[curr] < nums[left] && nums[curr] < nums[right])) {
                 count++;
             }
 
