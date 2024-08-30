9bbf05e98ded1636e59a826328dd6d062edeb685
Vinayak Shukla <vinayakshukla0786@gmail.com>
2024-08-30 10:46:23 +0530

    Time: 4 ms (71.36%), Space: 43.2 MB (71.85%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        int ch[]=new int[26];
+        for(int i=0;i<s.length();i++)
+            ch[s.charAt(i)-'a']++;
+
+        
+        for(int i=0;i<t.length();i++)
+            ch[t.charAt(i)-'a']--;
+     
+        for(int i:ch)
+        if(i!=0)
+        return false;
+      
+   
+        return true;
+    }
+}
