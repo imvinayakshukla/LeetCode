fc3790f0a2c9b03209056fda6e4a74e5d24ac2c3
Vinayak Shukla <vinayakshukla0786@gmail.com>
2024-08-30 10:53:00 +0530

    Time: 0 ms (100%), Space: 41.4 MB (39.28%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.java
+++ 0812-rotate-string/0812-rotate-string.java
@@ -1,12 +1,15 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
+
         if(s.length()!=goal.length())
         return false;
-
         if(s.compareTo(goal)==0)
+        return true;
+
+        if((s+s).indexOf(goal)>=1)
             return true;
 
-        return (s+s).indexOf(goal)>=1;
+        return false;
         
     }
 }
