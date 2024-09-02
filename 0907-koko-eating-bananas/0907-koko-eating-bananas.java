f64cf47bde68d39cd4f4d38cc23955d83d89fd50
Vinayak Shukla <vinayakshukla0786@gmail.com>
2024-09-02 21:35:26 +0530

    Time: 27 ms (25.62%), Space: 45 MB (48.39%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,33 +1,38 @@
-
 class Solution {
+    public boolean func(int mid,int[]piles,int h)
+    {
 
-    static int findth(int piles[], int h) {
-        int sum = 0;
-        for (int i = 0; i < piles.length; i++) {
+        int sum=0;
+        for(int i=0;i<piles.length;i++)
+        {
+            sum+=(Math.ceil((double)piles[i]/(double)mid));
 
-            sum += Math.ceil((double) piles[i] / (double) h);
         }
-        return sum;
+        if(sum<=h)
+        return true;
+        return false;
+
     }
-
     public int minEatingSpeed(int[] piles, int h) {
-        int low = 1, high = piles[0];
-        for (int i = 0; i < piles.length; i++) {
-            high = Math.max(high, piles[i]);
 
-        }
+        int low=0,high=piles[0];
+        for(int i:piles)
+        high=Math.max(high,i);
         System.out.println(high);
 
-        while (low <= high) {
-            int mid = (high + low) / 2;
-            int th = findth(piles, mid);
-            if (th <= h)
-
-                high = mid - 1;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            if(func(mid,piles,h))
+                high=mid-1;
             else
-                low = mid + 1;
-        }
-        return low;
+                low=mid+1;
+            
 
+
+        }
+
+        return low;
+        
     }
 }
