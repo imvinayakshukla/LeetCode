9fee149c90564d6fcb413bb451c4e72090697030
Vinayak Shukla <vinayakshukla0786@gmail.com>
2024-09-02 22:12:19 +0530

    Time: 12 ms (31.18%), Space: 46.8 MB (92.92%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
@@ -1,40 +1,38 @@
 class Solution {
-    static boolean check(int capacity, int weights[], int D) {
+    public boolean func(int mid, int[] weights, int days) {
         int day = 1, total = 0;
+        for (int i = 0; i < weights.length; i++) {
+            if (weights[i] + total > mid) {
+                day += 1;
+                total = weights[i];
+            } else
+                total += weights[i];
 
-        for(int i=0;i<weights.length;i++)
-        {
-            if(weights[i]+total>capacity)
-            {
-                day+=1;
-                total=weights[i];
-            }
-            else
-            total+=weights[i];
         }
-        if(day>D)
-        return false;
-        return true;
-
+        if (day > days)
+            return false;
+            return true;
+       
     }
 
     public int shipWithinDays(int[] weights, int days) {
-        int low = Integer.MIN_VALUE, high = 0;
-        for (int i = 0; i < weights.length; i++) {
-            high += weights[i];
-            low = Math.max(low, weights[i]);
+        int low = Integer.MIN_VALUE;
+        int high = 0;
+
+        for (int i : weights) {
+            low = Math.max(low, i);
+            high += i;
         }
 
         while (low <= high) {
-            int mid = (low + (high - low) / 2);
-            if (check(mid, weights, days))
+            int mid = (low + high) / 2;
+
+            if (func(mid, weights, days))
                 high = mid - 1;
             else
                 low = mid + 1;
-
         }
 
         return low;
-
     }
 }
