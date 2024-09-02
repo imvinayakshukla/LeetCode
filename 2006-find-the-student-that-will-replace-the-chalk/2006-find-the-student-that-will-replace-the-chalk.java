e1d6c8f6ccdf7c2998ffb3498660133ad6963d94
Vinayak Shukla <vinayakshukla0786@gmail.com>
2024-09-02 14:11:59 +0530

    Time: 2 ms (72.59%), Space: 60.3 MB (20.62%) - LeetHub

--- 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.java
+++ 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.java
@@ -1,21 +1,18 @@
 class Solution {
     public int chalkReplacer(int[] chalk, int k) {
-        // Step 1: Calculate total chalk needed for one complete round
         long totalChalk = 0;
         for (int c : chalk) {
             totalChalk += c;
         }
         
-        // Step 2: Reduce k modulo totalChalk
         k %= totalChalk;
 
-        // Step 3: Use binary search to find the first student who can't be given the chalk
         int low = 0, high = chalk.length - 1;
 
         while (low <= high) {
             int mid = (low + high) / 2;
             
-            if (func(chalk, k, mid)) {  // Replace this with the appropriate condition
+            if (func(chalk, k, mid)) { 
                 high = mid - 1;
             } else {
                 low = mid + 1;
@@ -25,8 +22,7 @@ class Solution {
         return low;
     }
     
-    // Implement the func method, if needed
-    private boolean func(int[] chalk, int k, int mid) {
+    public boolean func(int[] chalk, int k, int mid) {
         for (int i = 0; i <= mid; i++) {
             k -= chalk[i];
             if (k < 0) {
