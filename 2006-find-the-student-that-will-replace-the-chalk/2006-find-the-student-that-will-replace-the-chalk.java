5676cab9588a83e5cb12306d5c93428a3cdf2fe3
Vinayak Shukla <vinayakshukla0786@gmail.com>
2024-09-02 14:11:36 +0530

    Time: 2 ms (72.59%), Space: 60.3 MB (20.62%) - LeetHub

--- 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.java
+++ 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.java
@@ -1,24 +1,38 @@
 class Solution {
     public int chalkReplacer(int[] chalk, int k) {
-
-        if(chalk.length==1)
-            return 0;
-
-            long sum=0;
-            for(int i=0;i<chalk.length;i++)
-                sum+=chalk[i];
-
-                long r=k%sum;
-                System.out.println(k);
-
-        for(int i=0;i<chalk.length;i=(i+1)%(chalk.length))
-        {
-            if(r<chalk[i])
-                return i;
-
-            r=r-chalk[i];
+        // Step 1: Calculate total chalk needed for one complete round
+        long totalChalk = 0;
+        for (int c : chalk) {
+            totalChalk += c;
         }
-        return r;
         
+        // Step 2: Reduce k modulo totalChalk
+        k %= totalChalk;
+
+        // Step 3: Use binary search to find the first student who can't be given the chalk
+        int low = 0, high = chalk.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            
+            if (func(chalk, k, mid)) {  // Replace this with the appropriate condition
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+    
+    // Implement the func method, if needed
+    private boolean func(int[] chalk, int k, int mid) {
+        for (int i = 0; i <= mid; i++) {
+            k -= chalk[i];
+            if (k < 0) {
+                return true;
+            }
+        }
+        return false;
     }
 }
